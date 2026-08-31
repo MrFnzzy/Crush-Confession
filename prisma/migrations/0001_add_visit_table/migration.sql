d16f331c9a676e0142a598df7f3027b005c78127
MrFnzzy <149666707+MrFnzzy@users.noreply.github.com>
2026-08-31 18:00:59 +0800

    vew

--- prisma/migrations/0001_add_visit_table/migration.sql
+++
@@ -1,6 +0,0 @@
--- Add the anonymous visit counter table.
-CREATE TABLE IF NOT EXISTS "Visit" (
-  "id" SERIAL NOT NULL,
-  "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT "Visit_pkey" PRIMARY KEY ("id")
-);
